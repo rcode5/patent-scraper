0104269f4dabeb0599aceb539ec9fe09319ea334
Mr Rogers <jon@bunnymatic.com>
2015-08-18 20:44:58 -0700

    pull out some data

--- app/services/fetcher.rb
+++ app/services/fetcher.rb
@@ -2,8 +2,10 @@ require 'open-uri'
 class Fetcher
   def self.fetch(url, opts = {})
     doc = Nokogiri::HTML.parse(open(url).read)
-    doc.search(".//img").remove if opts[:remove_images]
-    doc.search(".//script").remove if opts[:remove_scripts]
+    tags_to_remove = opts[:remove_tags] || []
+    tags_to_remove.each do |tag|
+      doc.search(".//#{tag}").remove
+    end
     doc
   end
 end
--- app/services/patent_scraper_service.rb
+++ app/services/patent_scraper_service.rb
@@ -1,10 +1,55 @@
 class PatentScraperService
   attr_reader :page
   def initialize(patent_url)
-    @page = Fetcher.fetch(patent_url, remove_images: true, remove_scripts: true)
+    @page = Fetcher.fetch(patent_url, remove_tags: ['hr', 'img', 'br', 'script'])
   end
 
   def data
+    {
+      title: title,
+      patent: patent
+    }.merge(headers)
+  end
+
+  def text_with_header(header)
+    # find paragraph following header
+    p = header.next_element
+    p.text if p
+  end
+  
+  def title
+    @title ||= @page.css('font[size="+1"]').first.try(:text).try(:strip)
+  end
+
+  def headers
+    @headers ||=
+      begin
+        {}.tap do |headers|
+          @page.css('center').each do |header|
+            key = header.text.strip
+            if key.present?
+              value = text_with_header(header)
+              headers[key] = value if value.present?
+            end
+          end
+        end
+      end
+  end
+  
+  def patent
+    @patent ||= rows(tables[2]).map{|row| cells(row).map{|r| r.text.strip}}
+  end
+
+  def tables
+    @tables = @page.css('table')
+  end
+
+  def rows(table)
+    table.css('tr')
+  end
+
+  def cells(row)
+    row.css('td')
   end
 
 end
